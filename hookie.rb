b8d69c6dcb119941c15de532a553768c06c09956
Pieter Joost van de Sande <pj@born2code.net>
2016-11-17 01:37:20 +0100

    add hookie formula

---
+++ hookie.rb
@@ -0,0 +1,12 @@
+class Hookie < Formula
+  desc "a simple server that turns webhook calls into command execution"
+  homepage "https://github.com/pjvds/hookie"
+  url "https://github.com/pjvds/hookie/releases/download/v1.1/hookie-v1.1-darwin-amd64.tar.gz"
+  sha256 "055b4d73ec069b35eca41532cbf241b6a5a644b92c5008b0caaf8f0d92d2ca25"
+
+  bottle :unneeded
+
+  def install
+	bin.install "hookie"
+  end
+end
